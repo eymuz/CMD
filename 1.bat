5f3565560e4f466e3c193dc6e04c29b62fe4ba01
Artun <artunzeytun@gmail.com>
2025-06-15 12:49:41 +0300

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,52 @@
+start microsoft-edge:
+nircmd wait 2000
+nircmd sendkeypress ctrl+1
+nircmd wait 1000
+nircmd sendkeypress d
+nircmd wait 500
+nircmd sendkeypress w
+nircmd wait 500
+nircmd sendkeypress a
+nircmd wait 500
+nircmd sendkeypress s
+nircmd wait 500
+nircmd sendkeypress a
+nircmd wait 500
+nircmd sendkeypress w
+nircmd wait 500
+nircmd sendkeypress a
+nircmd wait 500
+nircmd sendkeypress s
+nircmd wait 500
+nircmd sendkeypress d
+nircmd wait 500
+nircmd sendkeypress w
+nircmd wait 500
+nircmd sendkeypress d
+nircmd wait 500
+nircmd sendkeypress s
+nircmd wait 500
+nircmd sendkeypress d
+nircmd wait 500
+nircmd sendkeypress w
+nircmd wait 500
+nircmd sendkeypress a
+nircmd wait 500
+nircmd sendkeypress s
+nircmd wait 500
+nircmd sendkeypress d
+nircmd wait 500
+nircmd sendkeypress w
+nircmd wait 500
+nircmd sendkeypress a
+nircmd wait 500
+nircmd sendkeypress w
+nircmd wait 500
+nircmd sendkeypress d
+nircmd wait 500
+nircmd sendkeypress w
+nircmd wait 500
+
+
+
+
